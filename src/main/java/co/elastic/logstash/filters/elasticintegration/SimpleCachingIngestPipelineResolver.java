e97e8722f3834642d1d4781a4b5ad129bfd36d7d
Ry Biesemeyer <ry.biesemeyer@elastic.co>
2023-02-10 19:43:27 +0000

    component: pipeline processor invoking our IngestPipeline
     - Introduces an `ingest.PipelineProcessor` that "quacks like" the pipeline
       processor from Elasticsearch ingest, except that it resolves named
       pipelines _through_ a provided `IngestPipelineResolver`.
     - Provides a way to get an immutabe copy of `IngestPipelineFactory` with
       an `ingest.PipelineProcessor.Factory` that is associated with
       a given `IngestPipelineResolver` so that it can look up pipelines
     - `SimpleIngestPipelineResolver` is modified to implement
       `SimpleCachingResolver.Bindable`, so that a caching wrapper's
       cashe-miss resolver will use an `IngestPipelineFactory` that
       is bound to the caching wrapper, so that any instances of
       `ingest.PipelineProcessor` can look up sub-pipelines _through_ the
       cache.
     - Extends Smoke Tests to verify multi-pipeline setups

--- src/main/java/co/elastic/logstash/filters/elasticintegration/SimpleCachingIngestPipelineResolver.java
+++ src/main/java/co/elastic/logstash/filters/elasticintegration/SimpleCachingIngestPipelineResolver.java
@@ -4,7 +4,7 @@ import co.elastic.logstash.filters.elasticintegration.resolver.ResolverCache;
 import co.elastic.logstash.filters.elasticintegration.resolver.SimpleCachingResolver;
 
 public class SimpleCachingIngestPipelineResolver extends SimpleCachingResolver<String, IngestPipeline> implements IngestPipelineResolver {
-    public SimpleCachingIngestPipelineResolver(final ResolverCache<String, IngestPipeline> cache, final SimpleIngestPipelineResolver cacheMissResolver) {
-        super(cache, cacheMissResolver);
+    public SimpleCachingIngestPipelineResolver(final ResolverCache<String, IngestPipeline> cache, final SimpleIngestPipelineResolver bindableCacheMissResolver) {
+        super(cache, (Bindable<String, IngestPipeline>) bindableCacheMissResolver);
     }
 }
