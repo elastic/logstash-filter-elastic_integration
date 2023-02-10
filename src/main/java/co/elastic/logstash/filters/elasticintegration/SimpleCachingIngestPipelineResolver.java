2a77022ae17368f96d4cdf6f043bba230dc5bead
Ry Biesemeyer <ry.biesemeyer@elastic.co>
2023-02-10 19:40:05 +0000

    component: ingest-pipeline-resolver
    Introduces an `IngestPipelineResolver` interface for resolving executable
    `IngestPipeline` instances from pipeline names, along with a simple
    implementation that combines a `PipelineConfigurationResolver` with an
    `IngestPipelineFactory`.

---
+++ src/main/java/co/elastic/logstash/filters/elasticintegration/SimpleCachingIngestPipelineResolver.java
@@ -0,0 +1,10 @@
+package co.elastic.logstash.filters.elasticintegration;
+
+import co.elastic.logstash.filters.elasticintegration.resolver.ResolverCache;
+import co.elastic.logstash.filters.elasticintegration.resolver.SimpleCachingResolver;
+
+public class SimpleCachingIngestPipelineResolver extends SimpleCachingResolver<String, IngestPipeline> implements IngestPipelineResolver {
+    public SimpleCachingIngestPipelineResolver(final ResolverCache<String, IngestPipeline> cache, final SimpleIngestPipelineResolver cacheMissResolver) {
+        super(cache, cacheMissResolver);
+    }
+}
