dfcb5c0233c77c9c09fbd72c59859a84c213edaa
Ry Biesemeyer <ry.biesemeyer@elastic.co>
2023-02-10 19:26:43 +0000

    component: pipeline-configuration-resolver
    Introduces the `PipelineConfigurationResolver` interface for resolving
    Elasticsearch-ingest `PipelineConfiguration`s from their pipeline names,
    along with a simple implementation that safely loads pipeline definitions
    from a local directory.

---
+++ src/test/java/co/elastic/logstash/filters/elasticintegration/LocalDirectoryPipelineConfigurationResolverTest.java
@@ -0,0 +1,101 @@
+package co.elastic.logstash.filters.elasticintegration;
+
+import org.elasticsearch.ingest.PipelineConfiguration;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static co.elastic.logstash.filters.elasticintegration.util.LocalPipelinesUtil.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class LocalDirectoryPipelineConfigurationResolverTest {
+    @Test
+    void resolvePresent() {
+        final Path pipelinesResourcePath = getPreparedPipelinesResourcePath(this.getClass(), "simple-mutate-pipelines");
+
+        final PipelineConfigurationResolver pcr = new LocalDirectoryPipelineConfigurationResolver(pipelinesResourcePath);
+
+        final Optional<PipelineConfiguration> resolved = pcr.resolve("simple-mutate");
+        assertThat(resolved.isPresent(), is(true));
+        assertThat(resolved.get().getId(), is(equalTo("simple-mutate")));
+    }
+
+    @Test
+    void resolveMissing() {
+        final Path pipelinesResourcePath = getPreparedPipelinesResourcePath(this.getClass(), "simple-mutate-pipelines");
+
+        final PipelineConfigurationResolver pcr = new LocalDirectoryPipelineConfigurationResolver(pipelinesResourcePath);
+
+        final Optional<PipelineConfiguration> resolved = pcr.resolve("not-there");
+        assertThat(resolved.isPresent(), is(false));
+    }
+
+    @Test
+    void resolvePresentNonReadable() {
+        final Path pipelinesResourcePath = getPreparedPipelinesResourcePath(this.getClass(), "simple-mutate-pipelines");
+        ensureSetFileReadable(pipelinesResourcePath.resolve("simple-mutate.json").toFile(), false);
+
+        final PipelineConfigurationResolver pcr = new LocalDirectoryPipelineConfigurationResolver(pipelinesResourcePath);
+
+        final Optional<PipelineConfiguration> resolved = pcr.resolve("simple-mutate");
+        assertThat(resolved, is(notNullValue()));
+        assertThat(resolved.isPresent(), is(false));
+
+        final AtomicReference<Exception> lastExceptionObserved = new AtomicReference<>();
+        final Optional<PipelineConfiguration> resolvedObserved = pcr.resolve("simple-mutate", lastExceptionObserved::set);
+        assertAll("observed resolve", ()-> {
+                    assertThat(resolvedObserved.isPresent(), is(false));
+                    assertThat(lastExceptionObserved.get(), is(notNullValue()));
+                    assertAll("exception observed", () -> {
+                        assertThat(lastExceptionObserved.get(), is(instanceOf(IOException.class)));
+                        assertThat(lastExceptionObserved.get().getMessage(), stringContainsInOrder("simple-mutate.json", "must be readable"));
+                    });
+                });
+
+        // recovery
+        ensureSetFileReadable(pipelinesResourcePath.resolve("simple-mutate.json").toFile(), true);
+        final Optional<PipelineConfiguration> resolvedRecovered = pcr.resolve("simple-mutate");
+        assertAll("recovery", () -> {
+            assertThat(resolvedRecovered, is(notNullValue()));
+            assertThat(resolvedRecovered.isPresent(), is(true));
+            assertThat(resolvedRecovered.get().getId(), is(equalTo("simple-mutate")));
+        });
+    }
+
+    @Test
+    void resolvePresentWritable() {
+        final Path pipelinesResourcePath = getPreparedPipelinesResourcePath(this.getClass(), "simple-mutate-pipelines");
+        ensureSetFileWritable(pipelinesResourcePath.resolve("simple-mutate.json").toFile(), true);
+
+        final PipelineConfigurationResolver pcr = new LocalDirectoryPipelineConfigurationResolver(pipelinesResourcePath);
+
+        final Optional<PipelineConfiguration> resolved = pcr.resolve("not-there");
+        assertThat(resolved, is(notNullValue()));
+        assertThat(resolved.isPresent(), is(false));
+
+        final AtomicReference<Exception> lastExceptionObserved = new AtomicReference<>();
+        final Optional<PipelineConfiguration> resolvedObserved = pcr.resolve("simple-mutate", lastExceptionObserved::set);
+        assertAll("observed resolve", ()-> {
+            assertThat(resolvedObserved.isPresent(), is(false));
+            assertThat(lastExceptionObserved.get(), is(notNullValue()));
+            assertAll("exception observed", () -> {
+                assertThat(lastExceptionObserved.get(), is(instanceOf(IOException.class)));
+                assertThat(lastExceptionObserved.get().getMessage(), stringContainsInOrder("simple-mutate.json", "must not be writable"));
+            });
+        });
+
+        // recovery
+        ensureSetFileWritable(pipelinesResourcePath.resolve("simple-mutate.json").toFile(), false);
+        final Optional<PipelineConfiguration> resolvedRecovered = pcr.resolve("simple-mutate");
+        assertAll("recovery", () -> {
+            assertThat(resolvedRecovered, is(notNullValue()));
+            assertThat(resolvedRecovered.isPresent(), is(true));
+            assertThat(resolvedRecovered.get().getId(), is(equalTo("simple-mutate")));
+        });
+    }
+}
---
+++ src/test/java/co/elastic/logstash/filters/elasticintegration/util/LocalPipelinesUtil.java
@@ -0,0 +1,46 @@
+package co.elastic.logstash.filters.elasticintegration.util;
+
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.Optional;
+
+public class LocalPipelinesUtil {
+
+    public static Path getPreparedPipelinesResourcePath(final Class<?> resourceProvider, final String packageRelativePath) {
+        return getResourcePath(resourceProvider, packageRelativePath)
+                .map(LocalPipelinesUtil::ensureContentsReadableNonWritable)
+                .orElseThrow(() -> new IllegalArgumentException(String.format("failed to load resource for `%s`", packageRelativePath)));
+    }
+
+    static Optional<Path> getResourcePath(final Class<?> resourceProvider, final String packageRelativePath) {
+        return Optional.ofNullable(resourceProvider.getResource(packageRelativePath))
+                .map(URL::getPath)
+                .map(Paths::get);
+    }
+
+    static Path ensureContentsReadableNonWritable(Path path) {
+        for (File file : Objects.requireNonNull(path.toFile().listFiles())) {
+            ensureSetFileReadable(file, true);
+            ensureSetFileWritable(file, false);
+        }
+        return path;
+    }
+
+    public static void ensureSetFileReadable(File file, boolean desiredState) {
+        if (desiredState != file.canRead()) {
+            if (!file.setReadable(desiredState)) {
+                throw new IllegalStateException(String.format("failed to ensure readable=%s for file: %s", desiredState, file));
+            }
+        }
+    }
+    public static void ensureSetFileWritable(File file, boolean desiredState) {
+        if (desiredState != file.canWrite()) {
+            if (!file.setWritable(desiredState)) {
+                throw new IllegalStateException(String.format("failed to ensure writable=%s for file: %s", desiredState, file));
+            }
+        }
+    }
+}
