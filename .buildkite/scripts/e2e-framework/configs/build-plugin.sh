bf912e8d9dfd89290a4c7cc290ffcff0f0143175
Mashhur <mashhur.sattorov@elastic.co>
2023-05-14 12:32:53 -0700

    Build plugin with gradle instead bundler.

--- .buildkite/scripts/e2e-framework/configs/build-plugin.sh
+++ .buildkite/scripts/e2e-framework/configs/build-plugin.sh
@@ -7,7 +7,9 @@ cd .. && cd ..
 echo "Current dir:" $PWD
 echo "List of files: " . $(ls)
 
-bundle install
-bundle exec rake prepare_geoip_resources
-bundle exec rake install_jars
+./gradlew clean vendor geoipTestResources localGem
+
+#bundle install
+#bundle exec rake prepare_geoip_resources
+#bundle exec rake install_jars
 cd .buildkite && cd scripts
