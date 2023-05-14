7443ffaedabc71a2c843fea2f8712520147e067a
Mashhur <mashhur.sattorov@elastic.co>
2023-05-14 10:32:44 -0700

    Changing working dir.

--- .buildkite/scripts/e2e_tests.sh
+++ .buildkite/scripts/e2e_tests.sh
@@ -1,4 +1,6 @@
 # TODO: download e2e-framework from GH when prototype is ready
+cd .buildkite/scripts
+
 ./e2e-framework/e2e_framework_run.sh $1 # platform, either macos or debian
 
 # TODO: create a python script to run E2E tests for elastic_integration plugin use-cases
