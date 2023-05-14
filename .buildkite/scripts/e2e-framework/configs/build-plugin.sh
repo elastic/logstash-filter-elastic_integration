585780c9bac2858cb62a30adc52445b0bd560134
Mashhur <mashhur.sattorov@elastic.co>
2023-05-14 12:26:53 -0700

    Debugging.

--- .buildkite/scripts/e2e-framework/configs/build-plugin.sh
+++ .buildkite/scripts/e2e-framework/configs/build-plugin.sh
@@ -2,10 +2,11 @@
 
 set -e
 
+cd .. && cd ..
+
 echo "Current dir:" $PWD
 echo "List of files: " . $(ls)
 
-cd .. && cd ..
 bundle install
 bundle exec rake prepare_geoip_resources
 bundle exec rake install_jars
