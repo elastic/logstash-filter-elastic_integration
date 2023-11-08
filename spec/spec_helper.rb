d5938fa99bf1826086879a78a48a23b2cf12cfce
Ry Biesemeyer <ry.biesemeyer@elastic.co>
2023-11-08 02:07:09 +0000

    specs: add x-pack to load path since we cannot rely on LS::Runner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,16 @@ require "rspec"
 require "rspec/mocks"
 require "logstash/devutils/rspec/spec_helper"
 
+# Since we use Logstash's x-pack WITHOUT the LogStash::Runner,
+# we must find it relative to logstash-core and add it to the load path.
+require 'pathname'
+logstash_core_path = Gem.loaded_specs['logstash-core']&.full_gem_path or fail("logstash-core lib not found")
+logstash_xpack_load_path = Pathname.new(logstash_core_path).join("../x-pack/lib").cleanpath.to_s
+unless $LOAD_PATH.include?(logstash_xpack_load_path)
+  $stderr.puts("ADDING LOGSTASH X-PACK to load path: #{logstash_xpack_load_path}")
+  $LOAD_PATH.unshift(logstash_xpack_load_path)
+end
+
 module SpecHelper
 
   def get_host_port_for_es_client
