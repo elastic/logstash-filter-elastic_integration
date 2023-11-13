05af4e10f4613c53dc7df28641d3d1cf51c919c9
Mashhur <mashhur.sattorov@elastic.co>
2023-11-14 02:41:26 +0500

    A plugin build pipeline which builds the plugin on various ES versions including snapshots and main branch to detect the issues earlies.

---
+++ .buildkite/scripts/resolve_stack_version.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This script resolves latest version from VERSION_URL SNAPSHOTS based on given N.x (where N is a precise, ex 8.x)
+# Why Snapshot? - the 7.latest and 8.latest branchs will be accurately places in snapshots, not in releases.
+# Ensure you have set the ELASTIC_STACK_VERSION environment variable.
+
+set -e
+
+VERSION_URL="https://raw.githubusercontent.com/elastic/logstash/main/ci/logstash_releases.json"
+
+echo "Fetching versions from $VERSION_URL"
+VERSIONS=$(curl --silent $VERSION_URL)
+
+if [[ "$SNAPSHOT" == "true" ]]; then
+  key='.snapshots | .[]'
+else
+  key='.releases | .[]'
+fi
+
+VERSION_KEYS=$(echo "$VERSIONS" | jq -r $key)
+
+ELASTIC_STACK_VERSIONS=()
+while IFS= read -r line; do
+  ELASTIC_STACK_VERSIONS+=("$line")
+  echo "Resolved version: $line"
+done <<< "$VERSION_KEYS"
