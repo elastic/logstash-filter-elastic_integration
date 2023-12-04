3a439624e3aae256aeccfa10a21906a9cd4e1477
Mashhur <mashhur.sattorov@elastic.co>
2023-12-04 09:59:53 -0800

    Absorbing ELASTICSEARCH_TREEISH resolve script into running tests script to avoid multiple script calls in the BK pipeline definition.

--- .buildkite/scripts/run_tests.sh
+++ .buildkite/scripts/run_tests.sh
@@ -1,1 +1,8 @@
+if [ -z "$ELASTICSEARCH_TREEISH" ]; then
+  source .buildkite/scripts/resolve_es_branch.sh
+  echo "Resolved ELASTICSEARCH_TREEISH: ${ELASTICSEARCH_TREEISH}"
+else
+  echo "ELASTICSEARCH_TREEISH ${ELASTICSEARCH_TREEISH} is defined."
+fi
+
 mkdir -p .ci && curl -sL https://github.com/logstash-plugins/.ci/archive/buildkite-1.x.tar.gz | tar zxvf - --skip-old-files --strip-components=1 -C .ci --wildcards '*Dockerfile*' '*docker*' '*.sh' && .ci/docker-setup.sh && .ci/docker-run.sh
