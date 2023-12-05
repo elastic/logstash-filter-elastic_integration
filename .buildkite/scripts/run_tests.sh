afa65dc41ea10e4678fa2aaf1361f585eeee8a52
Mashhur <mashhur.sattorov@elastic.co>
2023-12-05 10:53:12 -0800

    Pipeline definitions and shell script improvements.
    Co-authored-by: dliappis <dimitrios.liappis@elastic.co>

--- .buildkite/scripts/run_tests.sh
+++ .buildkite/scripts/run_tests.sh
@@ -1,8 +1,8 @@
 if [ -z "$ELASTICSEARCH_TREEISH" ]; then
-  source .buildkite/scripts/resolve_es_branch.sh
+  source .buildkite/scripts/resolve_es_treeish.sh
   echo "Resolved ELASTICSEARCH_TREEISH: ${ELASTICSEARCH_TREEISH}"
 else
-  echo "ELASTICSEARCH_TREEISH ${ELASTICSEARCH_TREEISH} is defined."
+  echo "Using ELASTICSEARCH_TREEISH ${ELASTICSEARCH_TREEISH} defined in the ENV."
 fi
 
-mkdir -p .ci && curl -sL https://github.com/logstash-plugins/.ci/archive/buildkite-1.x.tar.gz | tar zxvf - --skip-old-files --strip-components=1 -C .ci --wildcards '*Dockerfile*' '*docker*' '*.sh' && .ci/docker-setup.sh && .ci/docker-run.sh
+mkdir -p .ci && curl -sL --retry 5 --retry-delay 5 https://github.com/logstash-plugins/.ci/archive/buildkite-1.x.tar.gz | tar zxvf - --skip-old-files --strip-components=1 -C .ci --wildcards '*Dockerfile*' '*docker*' '*.sh' && .ci/docker-setup.sh && .ci/docker-run.sh
