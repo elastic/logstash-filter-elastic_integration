c8485e256a5b6f95a738f87d15a0336feb04d033
Mashhur <99575341+mashhurs@users.noreply.github.com>
2023-12-06 11:16:38 -0800

    Create build plugin pipeline (#103)
    * A plugin build pipeline which builds the plugin on various ES versions including snapshots and main branch to detect the issues earlies.
    
    * Use shared plugin CI rather building duplications. Apply idea suggestions of BK environment suuch as using non-root agent which already have JDK installed.
    
    * Add build pipeline definition in catalog info and organize the definitions.
    
    * Temporary file rename to test the changes.
    
    * Use proper agent to support docker operations, rename back PR pipeline after testing.
    
    * Absorbing ELASTICSEARCH_TREEISH resolve script into running tests script to avoid multiple script calls in the BK pipeline definition.
    
    * Pipeline definitions and shell script improvements.
    
    Co-authored-by: dliappis <dimitrios.liappis@elastic.co>
    
    * Set shell params to fast fail.
    
    Co-authored-by: dliappis <dimitrios.liappis@elastic.co>
    
    ---------
    
    Co-authored-by: dliappis <dimitrios.liappis@elastic.co>

---
+++ .buildkite/scripts/resolve_es_treeish.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+VERSION_URL="https://raw.githubusercontent.com/elastic/logstash/main/ci/logstash_releases.json"
+
+echo "Fetching versions from $VERSION_URL"
+VERSIONS=$(curl --retry 5 --retry-delay 5 -fsSL $VERSION_URL)
+
+if [[ "$SNAPSHOT" == "true" ]]; then
+  key=$(echo "$VERSIONS" | jq -r '.snapshots."'"$ELASTIC_STACK_VERSION"'"')
+else
+  key=$(echo "$VERSIONS" | jq -r '.releases."'"$ELASTIC_STACK_VERSION"'"')
+fi
+
+export ELASTICSEARCH_TREEISH=${key%.*}
