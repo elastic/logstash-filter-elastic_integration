1901afae192e3f63b150266e3d3e945ff1e12088
Mashhur <mashhur.sattorov@elastic.co>
2023-12-11 10:52:04 -0800

    Turn off unset variables on error option in the script.

--- .buildkite/scripts/resolve_es_treeish.sh
+++ .buildkite/scripts/resolve_es_treeish.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-set -euo pipefail
+set +u
 
 VERSION_URL="https://raw.githubusercontent.com/elastic/logstash/main/ci/logstash_releases.json"
 
 echo "Fetching versions from $VERSION_URL"
 VERSIONS=$(curl --retry 5 --retry-delay 5 -fsSL $VERSION_URL)
 
-snapshot=${SNAPSHOT:-false}
-
-if [[ "$snapshot" == "true" ]]; then
+if [[ "$SNAPSHOT" == "true" ]]; then
   key=$(echo "$VERSIONS" | jq -r '.snapshots."'"$ELASTIC_STACK_VERSION"'"')
 else
   key=$(echo "$VERSIONS" | jq -r '.releases."'"$ELASTIC_STACK_VERSION"'"')
