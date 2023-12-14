e510984d48e7df74c13d3abf11263feb444ead6f
Mashhur <mashhur.sattorov@elastic.co>
2023-12-14 07:58:26 -0800

    Set unset variable condirion just before checking snapshot.

--- .buildkite/scripts/resolve_es_treeish.sh
+++ .buildkite/scripts/resolve_es_treeish.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-set +u
+set -euo pipefail
 
 VERSION_URL="https://raw.githubusercontent.com/elastic/logstash/main/ci/logstash_releases.json"
 
 echo "Fetching versions from $VERSION_URL"
 VERSIONS=$(curl --retry 5 --retry-delay 5 -fsSL $VERSION_URL)
 
+set +o nounset
 if [[ "$SNAPSHOT" == "true" ]]; then
   key=$(echo "$VERSIONS" | jq -r '.snapshots."'"$ELASTIC_STACK_VERSION"'"')
 else
