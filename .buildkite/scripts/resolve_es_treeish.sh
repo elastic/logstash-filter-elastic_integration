a8a058dc5a7b13fbbc723bf1531123e1223d7e2d
João Duarte <jsvduarte@gmail.com>
2024-10-08 12:30:19 +0100

    updated jruby version to 9.4.8.0

--- .buildkite/scripts/resolve_es_treeish.sh
+++ .buildkite/scripts/resolve_es_treeish.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+set +o nounset
 if [[ "$SNAPSHOT" == "true" ]] && [[ "$ELASTIC_STACK_VERSION" == "8.x" ]]; then
   export ELASTICSEARCH_TREEISH=8.x
 else
@@ -10,7 +11,6 @@ else
   echo "Fetching versions from $VERSION_URL"
   VERSIONS=$(curl --retry 5 --retry-delay 5 -fsSL $VERSION_URL)
 
-  set +o nounset
   if [[ "$SNAPSHOT" == "true" ]]; then
     key=$(echo "$VERSIONS" | jq -r '.snapshots."'"$ELASTIC_STACK_VERSION"'"')
   else
