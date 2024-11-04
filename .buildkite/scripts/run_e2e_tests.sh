6a6df49eb77cffe8e8e722649204e59a3726d190
Mashhur <99575341+mashhurs@users.noreply.github.com>
2024-11-04 12:48:40 -0800

    Use target branch in build CI. Properly name the CI jobs. (#177)
    * Use target branch in build CI. Properly name the CI jobs.
    
    * Add unit test and integration tests on LS 8.x snapshot ES main branch.
    
    * Add the target branch to the E2E tests for LS 8.x
    
    * Add a step to E2E to cover LS snapshot vs ES main.
    
    * Use multi-jdk BK agent and use JDK version properly in E2E pipeline.
    
    * Add explanation comments for each jobs of each CI.

--- .buildkite/scripts/run_e2e_tests.sh
+++ .buildkite/scripts/run_e2e_tests.sh
@@ -2,26 +2,58 @@
 
 set -euo pipefail
 
-export PATH="/opt/buildkite-agent/.rbenv/bin:/opt/buildkite-agent/.pyenv/bin:/opt/buildkite-agent/.java/bin:$PATH"
-export JAVA_HOME="/opt/buildkite-agent/.java"
+export PATH="/opt/buildkite-agent/.rbenv/bin:/opt/buildkite-agent/.pyenv/bin:$PATH"
 eval "$(rbenv init -)"
 eval "$(pyenv init -)"
 
-VERSION_URL="https://storage.googleapis.com/artifacts-api/releases/current"
+VERSION_URL="https://raw.githubusercontent.com/elastic/logstash/main/ci/logstash_releases.json"
 
 ###
 # Resolve stack version and export
 resolve_current_stack_version() {
   set +o nounset
+  echo "Fetching versions from $VERSION_URL"
+  VERSIONS=$(curl --retry 5 --retry-delay 5 -fsSL $VERSION_URL)
 
-  local major_version="${ELASTIC_STACK_VERSION%%.*}"
-  local version=$(curl --retry 5 --retry-delay 5 -fsSL "$VERSION_URL/$major_version")
+  if [[ "$SNAPSHOT" == "true" ]]; then
+    key=$(echo "$VERSIONS" | jq -r '.snapshots."'"$ELASTIC_STACK_VERSION"'"')
+    echo "resolved key: $key"
+  else
+    key=$(echo "$VERSIONS" | jq -r '.releases."'"$ELASTIC_STACK_VERSION"'"')
+  fi
 
-  echo "Resolved version: $version"
-  export STACK_VERSION="$version"
+  echo "Resolved version: $key"
+  export STACK_VERSION="$key"
 }
 
-resolve_current_stack_version
+###
+# Checkout the target branch if defined
+checkout_target_branch() {
+  set +o nounset
+  if [ -z "$TARGET_BRANCH" ]; then
+    echo "Target branch is not specified, using default branch: main or BK defined"
+  else
+    echo "Changing the branch for ${TARGET_BRANCH}"
+    git checkout "$TARGET_BRANCH"
+  fi
+}
+
+set_required_jdk() {
+  set +o nounset
+  java_version="$(cat .java-version)"
+  echo "Required JDK version: $java_version"
+  if [[ "$java_version" == "17.0" ]]; then
+    jdk_home="/opt/buildkite-agent/.java/adoptiumjdk_17"
+  elif [[ "$java_version" == "21.0" ]]; then
+    jdk_home="/opt/buildkite-agent/.java/adoptiumjdk_21"
+  else
+    echo "Unsupported JDK."
+    exit 1
+  fi
+
+  export JAVA_HOME=$jdk_home
+  export PATH="$jdk_home:$PATH"
+}
 
 ###
 # Build the plugin, to do so we need Logstash source
@@ -41,6 +73,9 @@ build_plugin() {
   ./gradlew clean vendor localGem
 }
 
+resolve_current_stack_version
+checkout_target_branch
+set_required_jdk
 build_logstash
 build_plugin
 
