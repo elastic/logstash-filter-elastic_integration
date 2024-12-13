2cc7d7f14e05c87913ba99021e966dd6df376b99
Mashhur <99575341+mashhurs@users.noreply.github.com>
2024-12-13 11:05:27 -0800

    [8.x] Backport of #200, set ES treeish to 8.17 (#205)
    * [8.x] Backport of #190, dynamic CI pr/build/e2e jobs for target branch. (#200)
    
    * Sync up with ES main on `PipelineConfiguration` class changes. (#190)
    
    * Sync up with ES main that PipelineConfiguration class getConfigAsMap() method renamed to getConfig().
    
    * Request modifiable donfig when creating a pipeline.
    
    * Fixes PR CI jobs, adds build and E2E CI jobs.
    
    * Fix the E2E test jobs.
    
    * Generate CI steps based on defined test matrix.
    
    * Apply suggestions from code review
    
    - print target branch for debugging purpose
    - add return type `None` to void method.
    
    Co-authored-by: Cas Donoghue <cas.donoghue@gmail.com>
    
    * Replace the matrix with origin URL (after landing).
    
    ---------
    
    Co-authored-by: Cas Donoghue <cas.donoghue@gmail.com>
    (cherry picked from commit 8e4c02a53455f176d1f86589a22d53888b56094e)
    
    * Set ES treeish to 8.17

---
+++ .buildkite/scripts/e2e-pipeline/generate-steps.py
@@ -0,0 +1,86 @@
+import os
+import requests
+import sys
+import typing
+from requests.adapters import HTTPAdapter, Retry
+
+from ruamel.yaml import YAML
+
+RELEASES_URL = "https://raw.githubusercontent.com/elastic/logstash/main/ci/logstash_releases.json"
+TEST_COMMAND: typing.final = ".buildkite/scripts/run_e2e_tests.sh"
+
+
+def call_url_with_retry(url: str, max_retries: int = 5, delay: int = 1) -> requests.Response:
+    schema = "https://" if "https://" in url else "http://"
+    session = requests.Session()
+    # retry on most common failures such as connection timeout(408), etc...
+    retries = Retry(total=max_retries, backoff_factor=delay, status_forcelist=[408, 502, 503, 504])
+    session.mount(schema, HTTPAdapter(max_retries=retries))
+    return session.get(url)
+
+
+def generate_test_step(stack_version, es_treeish, snapshot) -> dict:
+    label_integration_test: typing.final = f"E2E tests for {stack_version}, snapshot: {snapshot}"
+    return {
+        "label": label_integration_test,
+        "command": TEST_COMMAND,
+        "env": {
+            "SNAPSHOT": snapshot,
+            "ELASTIC_STACK_VERSION": stack_version,
+            "ELASTICSEARCH_TREEISH": es_treeish
+        }
+    }
+
+
+if __name__ == "__main__":
+    structure = {
+        "agents": {
+            "provider": "gcp",
+            "machineType": "n2-standard-4",
+            "imageProject": "elastic-images-prod",
+            "image": "family/platform-ingest-logstash-multi-jdk-ubuntu-2204",
+            "diskSizeGb": 120
+        },
+        "steps": []}
+
+    steps = []
+    response = call_url_with_retry(RELEASES_URL)
+    versions_json = response.json()
+
+    # there are situations to manually run CIs with PR change,
+    # set MANUAL_TARGET_BRANCH with upstream target branch and run
+    manually_set_target_branch: typing.final = os.getenv("MANUAL_TARGET_BRANCH")
+    target_branch: typing.final = manually_set_target_branch if manually_set_target_branch else os.getenv("TARGET_BRANCH")
+    print(f"Running with target_branch: {target_branch}")
+    if target_branch == '8.x':
+        full_stack_version: typing.final = versions_json["snapshots"]["8.future"]
+        steps.append(generate_test_step(full_stack_version, target_branch, "true"))
+    elif target_branch == 'main':
+        full_stack_version: typing.final = versions_json["snapshots"][target_branch]
+        steps.append(generate_test_step(full_stack_version, target_branch, "true"))
+    else:
+        # generate steps for the version if released
+        releases = versions_json["releases"]
+        for release_version in releases:
+            if releases[release_version].startswith(target_branch):
+                steps.append(generate_test_step(releases[release_version], target_branch, "false"))
+                break
+
+        # steps for snapshot version
+        snapshots = versions_json["snapshots"]
+        for snapshot_version in snapshots:
+            if snapshots[snapshot_version].startswith(target_branch):
+                steps.append(generate_test_step(snapshots[snapshot_version], target_branch, "false"))
+                break
+
+    group_desc = f"{target_branch} branch E2E steps"
+    key_desc = "e2e-steps"
+    structure["steps"].append({
+        "group": group_desc,
+        "key": key_desc,
+        "steps": steps
+    })
+
+    print(
+        '# yaml-language-server: $schema=https://raw.githubusercontent.com/buildkite/pipeline-schema/main/schema.json')
+    YAML().dump(structure, sys.stdout)
---
+++ .buildkite/scripts/run_e2e_tests.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export PATH="/opt/buildkite-agent/.rbenv/bin:/opt/buildkite-agent/.pyenv/bin:$PATH"
+eval "$(rbenv init -)"
+eval "$(pyenv init -)"
+
+###
+# Set the Java home based on .java-version
+set_required_jdk() {
+  set +o nounset
+  java_version="$(cat .java-version)"
+  echo "Required JDK version: $java_version"
+  if [[ "$java_version" == "17.0" ]]; then
+    jdk_home="/opt/buildkite-agent/.java/adoptiumjdk_17"
+  elif [[ "$java_version" == "21.0" ]]; then
+    jdk_home="/opt/buildkite-agent/.java/adoptiumjdk_21"
+  else
+    echo "Unsupported JDK."
+    exit 1
+  fi
+
+  export JAVA_HOME=$jdk_home
+  export PATH="$jdk_home:$PATH"
+}
+
+###
+# Build the plugin, to do so we need Logstash source
+build_logstash() {
+  if [[ -d /usr/local/git-references/git-github-com-elastic-logstash-git ]]; then
+      retry -t 5 -- git clone -v --reference /usr/local/git-references/git-github-com-elastic-logstash-git -- https://github.com/elastic/logstash.git .
+  else
+      retry -t 5 -- git clone --single-branch https://github.com/elastic/logstash.git
+  fi
+
+  cd logstash && ./gradlew clean bootstrap assemble installDefaultGems && cd ..
+  LOGSTASH_PATH=$(pwd)/logstash
+  export LOGSTASH_PATH
+}
+
+build_plugin() {
+  ./gradlew clean vendor localGem
+}
+
+set_required_jdk
+build_logstash
+build_plugin
+
+###
+# Install E2E prerequisites and run E2E tests
+python3 -mpip install -r .buildkite/scripts/e2e-pipeline/requirements.txt
+python3 .buildkite/scripts/e2e-pipeline/main.py
