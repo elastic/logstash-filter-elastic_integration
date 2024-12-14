41de9b13735ade835d0686912d8beebc406b9cf1
Mashhur <mashhur.sattorov@elastic.co>
2024-12-13 16:24:39 -0800

    Fix the pipeline script crash.

--- .buildkite/scripts/build-pipeline/generate-steps.py
+++ .buildkite/scripts/build-pipeline/generate-steps.py
@@ -42,7 +42,7 @@ def generate_steps_for_scheduler(versions) -> list:
     steps: list = []
     snapshots = versions["snapshots"]
     for snapshot_version in snapshots:
-        if snapshots.get(snapshot_version,"").startswith("7."):
+        if snapshots[snapshot_version] is None or snapshots[snapshot_version].startswith("7."):
             continue
         full_stack_version = snapshots[snapshot_version]
         version_parts = snapshots[snapshot_version].split(".")
