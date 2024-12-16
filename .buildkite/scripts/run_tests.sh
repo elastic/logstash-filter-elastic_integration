65916db35e293cea3b4682c71a0094c2802c2a6f
Mashhur <99575341+mashhurs@users.noreply.github.com>
2024-12-16 09:25:55 -0800

    Backport PR-200 to 8.16 (#208)
    * [8.x] Backport of #190, dynamic CI pr/build/e2e jobs for target branch. (#200)
    
    * Sync up with ES main on `PipelineConfiguration` class changes. (#190)
    
    * Sync up with ES main that PipelineConfiguration class getConfigAsMap() method renamed to getConfig().
    
    * Request modifiable donfig when creating a pipeline.
    
    * Fixes PR CI jobs, adds build and E2E CI jobs.
    
    * Fix the E2E test jobs.
    
    * Generate CI steps based on defined test matrix.
    
    * Apply suggestions from code review
    
    - print target branch for debugging purpose
    - add return type `None` to void method.
    
    Co-authored-by: Cas Donoghue <cas.donoghue@gmail.com>
    
    * Replace the matrix with origin URL (after landing).
    
    ---------
    
    Co-authored-by: Cas Donoghue <cas.donoghue@gmail.com>
    (cherry picked from commit 8e4c02a53455f176d1f86589a22d53888b56094e)
    (cherry picked from commit 87c16ee1de04a8db5c251dbff400349939aaee32)
    
    * Set ES treeish and do not apply ES upstream interface changes.

--- .buildkite/scripts/run_tests.sh
+++ .buildkite/scripts/run_tests.sh
@@ -1,15 +1,1 @@
-if [ -z "$ELASTICSEARCH_TREEISH" ]; then
-  source .buildkite/scripts/resolve_es_treeish.sh
-  echo "Resolved ELASTICSEARCH_TREEISH: ${ELASTICSEARCH_TREEISH}"
-else
-  echo "Using ELASTICSEARCH_TREEISH ${ELASTICSEARCH_TREEISH} defined in the ENV."
-fi
-
-if [ -z "$TARGET_BRANCH" ]; then
-  echo "Target branch is not specified, using default branch: main or BK defined"
-else
-  echo "Changing the branch for ${TARGET_BRANCH}"
-  git checkout "$TARGET_BRANCH"
-fi
-
 mkdir -p .ci && curl -sL --retry 5 --retry-delay 5 https://github.com/logstash-plugins/.ci/archive/1.x.tar.gz | tar zxvf - --skip-old-files --strip-components=1 -C .ci --wildcards '*Dockerfile*' '*docker*' '*.sh' && .ci/docker-setup.sh && .ci/docker-run.sh
