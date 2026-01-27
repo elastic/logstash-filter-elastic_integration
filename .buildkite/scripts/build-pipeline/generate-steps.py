931f7a967420f656d03f5430d901e92812cf3aa4
Mashhur <mashhur.sattorov@elastic.co>
2026-01-27 13:44:41 -0800

    Resolve conflict and prepare 8.19.1 relase with a small error handling improvement.

--- .buildkite/scripts/build-pipeline/generate-steps.py
+++ .buildkite/scripts/build-pipeline/generate-steps.py
@@ -10,38 +10,6 @@ RELEASES_URL = "https://raw.githubusercontent.com/logstash-plugins/.ci/refs/head
 TEST_COMMAND: typing.final = ".buildkite/scripts/run_tests.sh"
 
 
-def generate_unit_and_integration_test_steps(stack_version, snapshot) -> list[typing.Any]:
-    test_steps = []
-
-    # step-1, unit tests
-    label_unit_test: typing.final = f"Unit test for {stack_version}, snapshot: {snapshot}"
-    test_steps.append({
-        "label": label_unit_test,
-        "command": TEST_COMMAND,
-        "env": {
-            "SNAPSHOT": snapshot,
-            "ELASTIC_STACK_VERSION": stack_version,
-            "INTEGRATION": "false"
-        }
-    })
-
-<<<<<<< HEAD
-    # step-2, integration tests
-    label_integration_test: typing.final = f"Integration test for {stack_version}, snapshot: {snapshot}"
-    test_steps.append({
-        "label": label_integration_test,
-        "command": TEST_COMMAND,
-        "env": {
-            "SNAPSHOT": snapshot,
-            "ELASTIC_STACK_VERSION": stack_version,
-            "INTEGRATION": "true",
-            "SECURE_INTEGRATION": "true",
-            "LOG_LEVEL": "info"
-        }
-    })
-    return test_steps
-
-
 def call_url_with_retry(url: str, max_retries: int = 5, delay: int = 1) -> requests.Response:
     schema = "https://" if "https://" in url else "http://"
     session = requests.Session()
@@ -49,7 +17,8 @@ def call_url_with_retry(url: str, max_retries: int = 5, delay: int = 1) -> reque
     retries = Retry(total=max_retries, backoff_factor=delay, status_forcelist=[408, 502, 503, 504])
     session.mount(schema, HTTPAdapter(max_retries=retries))
     return session.get(url)
-=======
+
+
 def generate_test_step(stack_version, branch, snapshot, test_type) -> dict:
     test_type_label = "Integration" if test_type == "integration" else "Unit"
     label_test: typing.final = f"{test_type_label} test for {stack_version}, snapshot: {snapshot}"
@@ -94,7 +63,6 @@ def generate_steps_for_main_branch(versions) -> list:
     steps.append(generate_test_step(full_stack_version, None, "true", "integration"))
     steps.append(generate_test_step(full_stack_version, None, "true", "unit"))
     return steps
->>>>>>> 603e6b2 (Include required hppc package, fix String.format issue and forwardport branch changelogs (#380))
 
 
 if __name__ == "__main__":
@@ -107,42 +75,27 @@ if __name__ == "__main__":
         },
         "steps": []}
 
-    steps = []
     response = call_url_with_retry(RELEASES_URL)
     yaml = YAML(typ='safe')
     versions_yaml: typing.final = yaml.load(response.text)
 
-    # there are situations to manually run CIs with PR change,
-    # set MANUAL_TARGET_BRANCH with upstream target branch and run
-    manually_set_target_branch: typing.final = os.getenv("MANUAL_TARGET_BRANCH")
-    target_branch: typing.final = manually_set_target_branch if manually_set_target_branch else os.getenv("TARGET_BRANCH")
-    print(f"Running with target_branch: {target_branch}")
-    if target_branch == '8.x':
-        full_stack_version: typing.final = versions_yaml["snapshots"]["8.future"]
-        steps += generate_unit_and_integration_test_steps(full_stack_version, "true")
-    elif target_branch == 'main':
-        full_stack_version: typing.final = versions_yaml["snapshots"][target_branch]
-        steps += generate_unit_and_integration_test_steps(full_stack_version, "true")
-    else:
-        # generate steps for the version if released
-        releases = versions_yaml["releases"]
-        for release_version in releases:
-            if releases[release_version].startswith(target_branch):
-                steps += generate_unit_and_integration_test_steps(releases[release_version], "false")
-                break
-        # steps for snapshot version
-        snapshots = versions_yaml["snapshots"]
-        for snapshot_version in snapshots:
-            if snapshots[snapshot_version].startswith(target_branch):
-                steps += generate_unit_and_integration_test_steps(snapshots[snapshot_version], "false")
-                break
+    # Use BUILDKITE_SOURCE to figure out PR merge or schedule.
+    # If PR merge, no need to run builds on all branches, target branch will be good
+    #   - webhook when PR gets merged
+    #   - schedule when daily schedule starts
+    #   - ui when manually kicking job from BK UI
+    #       - manual kick off will be on PR or entire main branch, can be decided with BUILDKITE_BRANCH
+    bk_source = os.getenv("BUILDKITE_SOURCE")
+    bk_branch = os.getenv("BUILDKITE_BRANCH")
+    steps = generate_steps_for_scheduler(versions_yaml) if (bk_source == "schedule" or bk_branch == "main") \
+        else generate_steps_for_main_branch(versions_yaml)
 
-    group_desc = f"{target_branch} branch steps"
-    key_desc = "pr-and-build-steps"
+    group_desc = f"Build steps"
+    key_desc = "build-steps"
     structure["steps"].append({
         "group": group_desc,
         "key": key_desc,
-        "steps": steps
+        "steps": steps,
     })
 
     print(
