e5948d6e61464c28c9d22cdd6264ac9423f11948
Mashhur <mashhur.sattorov@elastic.co>
2026-04-18 08:36:41 -0700

    Resolve conflicts.

--- .buildkite/scripts/run_tests.sh
+++ .buildkite/scripts/run_tests.sh
@@ -11,10 +11,6 @@ else
   git checkout "$TARGET_BRANCH"
 fi
 
-<<<<<<< HEAD
-mkdir -p .ci && curl -sL --retry 5 --retry-delay 5 https://github.com/logstash-plugins/.ci/archive/1.x.tar.gz | tar zxvf - --skip-old-files --strip-components=1 -C .ci --wildcards '*Dockerfile*' '*docker*' '*.sh' '*logstash-versions*' && .ci/docker-setup.sh && .ci/docker-run.sh
-=======
 ./utils/ssl-certs-generator/generate.sh
 chmod -R 0440 utils/ssl-certs-generator/generated/*
 mkdir -p .ci && curl -sL --retry 5 --retry-delay 5 https://github.com/logstash-plugins/.ci/archive/1.x.tar.gz | tar zxvf - --skip-old-files --strip-components=1 -C .ci --wildcards '*Dockerfile*' '*docker*' '*.sh' '*logstash-versions*' && .ci/docker-setup.sh && .ci/docker-run.sh
->>>>>>> 254e669 (Autorenew SSL certs and use them everywhere (#431))
